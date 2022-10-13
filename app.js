b5de0a1ef3345e360e193480ae9790558f88535a
moch.fathurrozi <moch.fathurrozi@ist.id>
2022-10-13 13:38:14 +0700

    adding env

--- app.js
+++ app.js
@@ -2,8 +2,10 @@ import express from "express";          // import express
 import db from './config/database.js'  // import db from database.js
 import productRouter from './routes/index.js'  // import router
 import cors from 'cors'
+import dotenv from 'dotenv'
 
 db.sync()
+dotenv.config()
 
 // set express
 const app = express()
@@ -22,4 +24,5 @@ app.use(express.json())                               // untuk menangkap request
 app.use('/users', productRouter)                     // maka routenya adalah /products
 
 // server connection
-app.listen(5000, () => console.info('Server now running at http://localhost:5000'))
+const port = process.env.PORT || 5000
+app.listen(port, () => console.info('Server now running at http://localhost:5000'))
--- config/database.js
+++ config/database.js
@@ -1,6 +1,9 @@
 import {Sequelize} from 'sequelize'
+import dotenv from 'dotenv'
 
-const db = new Sequelize('user_management', 'root', 'root123', { // 'db name' , 'username' , 'password' , {option}
+dotenv.config()
+
+const db = new Sequelize(process.env.MYSQL_DATABASE, process.env.MYSQL_USERNAME, process.env.MYSQL_PASSWORD, { // 'db name' , 'username' , 'password' , {option}
     host : "localhost",  // host address
     dialect : "mysql"    // db type
 })
